cb38083142203aea3a2e717334799380cdacd254
pablo flouret <quuxbaz@gmail.com>
2009-04-24 21:32:44 +0200

    main config file

--- lib/lijab/config.rb
+++ lib/lijab/config.rb
@@ -13,6 +13,7 @@ module Config
 
       setup_basedir(args[:basedir])
       read_accounts(args[:account])
+      read_options()
 
       @jid = Jabber::JID.new("#{@account[:jabberid]}")
       @jid.resource ||= "lijab#{(0...5).map{rand(10).to_s}.join}"
@@ -24,6 +25,10 @@ module Config
    def setup_basedir(basedir)
       xdg = ENV["XDG_CONFIG_HOME"]
       @basedir = basedir || xdg && File.join(xdg, "lijab") || File.expand_path("~/.lijab")
+
+      unless File.directory?(@basedir)
+         puts "Creating #{@basedir} with the default configs"
+      end
 
       %w{commands extensions logs}.each do |d|
          @dirs[d.to_sym] = path = File.join(@basedir, d)
@@ -64,6 +69,12 @@ module Config
       exit(1) unless errors.empty?
    end
 
+   def read_options
+      # FIXME: error check / validate
+      @opts = YAML.load(DEFAULT_FILES["config"])
+      @opts.merge!(YAML.load_file(@files[:config]))
+   end
+
    def create_account_log_dirs
       @accounts.select { |a| a[:log] }.each do |a|
          a[:log_dir] = File.join(@dirs[:logs], @jid.strip.to_s)
@@ -78,6 +89,7 @@ module Config
    DEFAULT_FILES = {
       "accounts" => %Q{
          # Accounts go here. Separate each one with ---
+         # First one is the default.
 
          #---
          #:name : an_account                  # the account name
@@ -94,7 +106,17 @@ module Config
 
       "config" => %Q{
          # default config file
-      }.gsub!(/^\s*/, '')
+         :datetime_format : %H:%M:%S          # time format for each message
+
+         # Command aliases.
+         # <command_alias> : <existing_command>
+         # Commands can be overloaded.
+         # For instance /who could be redefined like so to sort by status by default
+         # /who : /who status
+         :aliases :
+            /h : /history
+            /exit : /quit
+      }.gsub!(/^\s{9}/, '')
    }
 
    attr_reader     :jid, :account, :basedir, :dirs, :files, :opts
