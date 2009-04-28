93550dc27d30a9386f81e25736b272bc0f5c5efb
pablo flouret <quuxbaz@gmail.com>
2009-04-28 23:00:59 +0200

    groups in contact list

--- lib/lijab/commands/contacts.rb
+++ lib/lijab/commands/contacts.rb
@@ -12,6 +12,20 @@ module Commands
          end
       end
 
+      def group_contacts(contacts)
+         grouped = {}
+         contacts.each do |jid,contact|
+            groups = contact.roster_item.groups
+            if groups.empty?
+               (grouped["<no group>"] ||= []) << contact
+            else
+               groups.each { |g| (grouped[g] ||= []) << contact }
+            end
+         end
+
+         grouped = grouped.sort_by { |g,c| g }
+      end
+
       def print_contacts(sort_by_status=false, online_only=false)
          if sort_by_status
             contacts = Main.contacts.sort { |a, b| -(a[1].presence <=> b[1].presence) }
@@ -19,16 +33,30 @@ module Commands
             contacts = Main.contacts.sort_by { |j,c| c.simple_name }
          end
 
+         if Config.opts[:show_groups_in_contact_list]
+            grouped = group_contacts(contacts)
+         else
+            grouped = {nil => contacts.map { |j,c| c }}
+         end
+
          s = []
-         contacts.each do |jid,contact|
-            unless online_only && !contact.online?
-               main = contact.presence
-               s << "* #{contact.simple_name} #{main.pretty(true)} " \
-                    "(#{main.priority || 0}) [#{main.from || contact.jid}]"
-               if contact.roster_item.presences.length > 1
-                  contact.roster_item.presences.each do |p|
-                     if p.from != main.from
-                        s << "    #{p.from} #{p.pretty(true)} (#{p.priority || 0})"
+         grouped.each do |group, contactz|
+            if online_only
+               next unless contactz.any? { |c| c.online? }
+            end
+
+            s << " #{group} ".on_blue if group
+            contactz.each do |contact|
+               unless online_only && !contact.online?
+                  main = contact.presence
+                  s << "* #{contact.simple_name} #{main.pretty(true)} " \
+                     "(#{main.priority || 0}) [#{main.from || contact.jid}]"
+
+                  if online_only && contact.roster_item.presences.length > 1
+                     contact.roster_item.presences.each do |p|
+                        if p.from != main.from
+                           s << "    #{p.from} #{p.pretty(true)} (#{p.priority || 0})"
+                        end
                      end
                   end
                end
